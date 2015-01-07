1f43a2825182de4f18aab9a66b317ce476b23609
Jake Alvarez <jakealvarez81@hotmail.com>
2015-01-07 13:45:32 +0000

    added kitten pic to webpage

--- hello.rb
+++ hello.rb
@@ -1,10 +1,18 @@
 
 require 'sinatra'
 
+# get '/' do
+#   'Hello World2!'
+# end
+
 get '/' do
-  'Hello World!'
+  "<div>
+    <img src='http://bit.ly/1eze8aE'
+  </div>"
 end
+
 
 get '/secret' do
   'this is a secret page!'
 end
+
